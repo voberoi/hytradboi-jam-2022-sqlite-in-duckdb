9d6523dc88fe9e3370b9da1bdb2a2f57968ad56e
Vikram Oberoi <voberoi@gmail.com>
2022-09-26 11:54:59 -0400

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// The plan: create a SQLite DuckDB column type and play around with it.
+//
+// - Store SQLite dbs in DuckDB blobs.
+// - Provide functions to query these blobs.
+// - Play around with a few use cases and analyze them.
+// - Maybe provide other utility functions as I discove rthem.
+//
+// Currently need to:
+//
+// 1. Figure out how to write a simple DuckDB extension.
+// 2. Figure out how to write this simple DuckDB extension in Rust.
+
+fn main() {
+    println!("Hello, world!");
+}
